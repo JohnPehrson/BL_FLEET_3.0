322f277e51fd0071bac0cabaa9db0adaa7b34089
John C Pehrson <54759417+JohnPehrson@users.noreply.github.com>
2022-11-15 15:37:51 -0600

    Inner Variable and mean velo excel file for CFD

---
+++ RunComparison/InnerVariableCalculator.m
@@ -0,0 +1,40 @@
+function [u_plus_c,u_vd_plus_c,y_plus_c] = InnerVariableCalculator(u,y,Mach,T_inf,P_inf,U_inf)
+%This function calculates the inner variables given a number of inputs to
+%provide Van Driest corrected results. 
+%Assumes adiabatic walls, ideal gas, and measurements close enough to the
+%wall to directly measure the skin friction.
+
+%% Invariants
+Pr = 0.73;
+r = Pr^(1/3); %recovery percentage
+gamma = 1.4;
+R = 287; %J/kgK, gas constant
+%sutherland constants
+S = 110.4; %K
+T_ref = 273.15; %K
+mu_ref = 1.716*(10^-5); %kg/ms
+
+%change y to mm
+y = y./1000;
+
+%% Wall temperature
+T_w = (1+r*((gamma-1)/2).*(Mach^2)).*T_inf;
+
+%% Effective velocity using the Van Driest Scaling
+a = sqrt(1-T_inf/T_w);
+u_vd = (U_inf/a).*asin((a.*u)./U_inf);
+
+%% Wall viscosities
+rho_w = P_inf./(T_w.*R);
+mu_w = mu_ref.*((T_w/T_ref).^1.5).*((T_ref+S)/(T_w+S));
+v_w = mu_w./rho_w;
+
+%% Wall shear stress and u_tau
+    t_w_clauser = 12;
+    u_tau_c = sqrt(t_w_clauser/rho_w);
+
+%% Inner Variables
+u_plus_c = u./u_tau_c;
+u_vd_plus_c = u_vd./u_tau_c;
+y_plus_c = (y.*u_tau_c)./v_w;
+end
