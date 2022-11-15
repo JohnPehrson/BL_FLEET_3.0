f9d6fa42b05534a3d402d995e73739234fbea559
John C Pehrson <54759417+JohnPehrson@users.noreply.github.com>
2022-11-15 15:38:33 -0600

    Update VelocityFinder.m

--- RunComparison/VelocityFinder.m
+++ RunComparison/VelocityFinder.m
@@ -1,19 +1,19 @@
 function [velocity,random_velocity_error,systematic_velocity_error] = VelocityFinder(centroids,centroid_error,resolution,gates,delays)
 %This function finds the velocity of the flow based on the centroids.
 
-        resolution(1) = resolution(1)/(10^6);
+        res = resolution(1)/(10^6);
         t = [ delays(1)+gates(1)/2 ,delays(1)+gates(1)+delays(2)+gates(2)/2];
         dt = (t(2)-t(1))*10^(-9); %in seconds
         dt_err = 0; %no jitter between gates in a burst
        
         dx_pix = centroids(:,2)-centroids(:,1);
-        dx = dx_pix.*resolution(1); %displacement in meters
+        dx = dx_pix.*res; %displacement in meters
         velocity = dx./dt;
         dx_pix_err = sqrt(centroid_error(:,1).^2+centroid_error(:,2).^2);
         
-        reserr = resolution(2)/(10^5);
+        reserr = resolution(2)/(10^6);
         
-        random_du2 = (((resolution(1)/(dt)).^2).*(dx_pix_err).^2)+ (((dx_pix.*resolution(1)./(dt.^2)).^2).*(dt_err).^2);
+        random_du2 = (((res/(dt)).^2).*(dx_pix_err).^2)+ (((dx_pix.*res./(dt.^2)).^2).*(dt_err).^2);
         random_velocity_error = sqrt(random_du2);
         
         systematic_du2 = (((dx_pix./(dt)).^2).*(reserr)^2);
